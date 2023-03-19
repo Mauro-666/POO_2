9f5de586e0bb4d802b89638da2fa6fe5fa9cb4aa
Mauro-666 <eltercodesiempre73@gmail.com>
2023-03-19 14:38:45 -0300

    Primer commit

---
+++ src/clasesYobjetos/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasesYobjetos;
+
+// Creacion del Objeto
+
+public class Main {
+    public static void main(String[] args) {
+        Operacion op = new Operacion();
+        
+        op.leerNumeros();
+        op.suma();
+        op.resta();
+        op.division();
+        op.multiplicacion();
+        op.mostrarResultados();
+        
+    }
+    
+}
---
+++ src/clasesYobjetos/Operacion.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasesYobjetos;
+
+import javax.swing.JOptionPane;
+
+//Creacion de Atributos y Metodos
+
+
+public class Operacion {
+    
+    //Atributos
+    int numero1;
+    int numero2;
+    int suma;
+    int resta;
+    int division;
+    int multiplicacion;
+    
+    
+    //Metodos
+    
+    //Metodo para pedirle al usuario que ingrese dos numeros
+    public void leerNumeros(){
+    numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+    numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero"));
+    }
+    
+    //Metodos de cuenta
+    public void suma(){
+    suma = numero1+numero2;
+    }
+    public void resta(){
+    resta = numero1-numero2;
+    }
+    public void division(){
+    division = numero1/numero2;
+    }
+    public void multiplicacion(){
+    multiplicacion = numero1*numero2;
+    }
+    
+    //Metodo de resultados
+    public void mostrarResultados(){
+        System.out.println("La suma es: "+suma);
+        System.out.println("La resta es: "+resta);
+        System.out.println("La division es: "+division);
+        System.out.println("La multiplicacion es: "+multiplicacion);
+    }
+}
